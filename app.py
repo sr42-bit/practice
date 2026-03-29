8a75dd0cb8314b177dfb15009a8d902603936aa5
=papaji <=neenarathour1978@gmail.com>
2026-03-30 01:41:35 +0530

    add ci workflow

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import logging
+from operator import add
+
 logging.basicConfig(level=logging.INFO)
 
-def add(a, b):
-    logging.info("Addition operation performed")
-    return a + b
+def lambda_handler(event, context):
+    a = int(event.get("a", 0))
+    b = int(event.get("b", 0))
+
+    result = add(a, b)
+
+    return {
+        'statusCode': 200,
+        'body': f'Result is {result}'
+    }
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': 'Hello from Serverless!'
+    }
