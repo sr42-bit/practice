3cf888649668fb262e6fd954d308961b03389dbd
=papaji <=neenarathour1978@gmail.com>
2026-03-30 01:36:11 +0530

    add ci workflow

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-def add(a, b):
-    return a + b
+import logging
+logging.basicConfig(level=logging.INFO)
 
-if __name__ == "__main__":
-    print(add(2, 3))
+def add(a, b):
+    logging.info("Addition operation performed")
+    return a + b
